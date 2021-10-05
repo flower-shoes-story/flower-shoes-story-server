e8dd4f4fbdc06ba8101bc901d8807d09dd49adff
yerim jeon <wjsdpfla@gmail.com>
2021-10-06 08:27:15 +0900

    Feat: add constants

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+const TYPE = {
+  GOMSIN: "gomsin",
+  SOLDIER: "soldier",
+};
+
+const NUMBER = {
+  ONEDAY: 1000 * 60 * 60 * 24,
+};
+
+const MESSAGE = {
+  AUTHORIZED: "user authorized successfully",
+};
+
+module.exports = { NUMBER, MESSAGE, TYPE };
