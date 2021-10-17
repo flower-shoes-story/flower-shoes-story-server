6a992e323d36fd680a79bac853f232adadcb43d6
yerim jeon <79728856+yerim-jeon@users.noreply.github.com>
2021-10-17 21:12:57 +0900

    Revert "Feat: add event router"

--- routes/event.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { verifyToken } = require("../middlewares/verifyToken");
-const { getEvents } = require("../controllers/event.controller");
-
-router.get("/", verifyToken, getEvents);
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ var router = express.Router();
 
 const auth = require("./auth");
 const user = require("./user");
-const event = require("./event");
 
 router.use("/auth", auth);
 router.use("/user", user);
-router.use("/event", event);
 
 module.exports = router;
