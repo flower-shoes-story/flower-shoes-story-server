a87568b51588afb646471f2b7a0bb46c27351331
yerim jeon <wjsdpfla@gmail.com>
2021-10-17 20:50:11 +0900

    Style: add socket event constants

--- constants/index.js
+++ constants/index.js
@@ -13,8 +13,11 @@ const MESSAGE = {
 
 const EVENTS = {
   JOIN: "join",
-  GET_MESSAGE: "getMessage",
+  GET_MESSAGES: "getMessage",
   SEND_MESSAGE: "sendMessage",
+  SET_START_POSITION: "setStartPosition",
+  RESET_START_POSITION: "resetStartPosition",
+  LISTEN_JUMP_DIRECTION: "listenJumpDirection",
   DISCONNECT: "disconnect",
 };
 
