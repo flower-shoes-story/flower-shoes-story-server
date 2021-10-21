25c0467b79dbd7dca52fb42df107cb908b4a704a
yerim jeon <wjsdpfla@gmail.com>
2021-10-21 22:38:49 +0900

    Feat: add logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, authorize } = require("../controllers/auth.controller");
+const { login, logout, authorize } = require("../controllers/auth.controller");
 const { verifyToken } = require("../middlewares/verifyToken");
 
 router.post("/login", login);
+router.post("/logout", logout);
 router.get("/check", verifyToken, authorize);
 
 module.exports = router;
